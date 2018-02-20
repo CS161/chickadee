376acfda6e98c2db5296339fa7d4adecfa28936d
Eddie Kohler <ekohler@gmail.com>
2018-02-20 10:55:08 -0500

    Add p-testeintr.cc, answers/collab files

---
+++ p-testeintr.cc
@@ -0,0 +1,47 @@
+#include "p-lib.hh"
+
+static const int order[] = {
+    2, 7, 6, 4, 1, 8, 5, 3
+};
+
+static void make_children(pid_t* children) {
+    for (size_t i = 0; i != arraysize(order); ++i) {
+        pid_t p = sys_fork();
+        if (p == 0) {
+            sys_msleep(order[i] * 500);
+            sys_exit(order[i]);
+        }
+        children[i] = p;
+    }
+}
+
+void process_main() {
+    sys_kdisplay(KDISPLAY_NONE);
+
+    pid_t children[arraysize(order)];
+    make_children(children);
+    for (size_t i = 0; i != arraysize(order); ++i) {
+        int r = sys_msleep(1000);
+        assert(r == E_INTR);
+
+        int status = 0;
+        pid_t ch = sys_waitpid(0, &status, W_NOHANG);
+        assert(ch > 0);
+
+        size_t idx = 0;
+        while (idx != arraysize(order) && children[idx] != ch) {
+            ++idx;
+        }
+        assert(idx < arraysize(order));
+        children[idx] = 0;
+
+        console_printf("%d @%lu: exit status %d\n", ch, idx, status);
+        assert(order[idx] == status);
+    }
+    assert(sys_waitpid(0) == E_CHILD);
+    assert(sys_msleep(1000) == 0);
+
+
+    console_printf("testeintr succeeded.\n");
+    sys_exit(0);
+}
