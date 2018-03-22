21181ba21af59684e708e2e9cc1d296901c2d603
Eddie Kohler <ekohler@gmail.com>
2018-03-22 14:49:07 -0400

    Fix problem found by Dan Inge.

--- k-cpu.cc
+++ k-cpu.cc
@@ -138,6 +138,6 @@ void idle(proc*) {
 
 void cpustate::init_idle_task() {
     assert(!idle_task_);
-    idle_task_ = reinterpret_cast<proc*>(kallocpage());
+    idle_task_ = kalloc_proc();
     idle_task_->init_kernel(-1, idle);
 }
