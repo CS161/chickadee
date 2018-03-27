8cfad1148d78e05f8d670611ab20e8ddc4cfe31b
Eddie Kohler <ekohler@gmail.com>
2018-03-27 06:47:05 -0400

    You can decide how to use inode::mref.

--- k-chkfs.cc
+++ k-chkfs.cc
@@ -206,7 +206,6 @@ chickadeefs::inode* chkfsstate::get_inode(inum_t inum) {
     }
     if (ino != nullptr) {
         ino += inum % chickadeefs::inodesperblock;
-        ++ino->mref;
     }
     return ino;
 }
@@ -216,7 +215,6 @@ chickadeefs::inode* chkfsstate::get_inode(inum_t inum) {
 //    Drop the reference to `ino`.
 void chkfsstate::put_inode(inode* ino) {
     if (ino) {
-        --ino->mref;
         bufcache::get().put_block(ROUNDDOWN(ino, PAGESIZE));
     }
 }
