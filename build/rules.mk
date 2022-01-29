fceff778cd6365d75ff3ab4a6e7a65cb47010148
Abe Wieland <abe.wieland@gmail.com>
2022-01-28 19:25:21 -0500

    Transitioned `cc` and `c++` to `gcc` and `g++` when `CCPREFIX` is set; this means students who set `CCPREFIX` must no longer set `CC` and `CXX` as well.
    Removed `-nostartfiles` from `LD_FLAGS`. `-nostartfiles` is actually
    a `gcc` option, not an `ld` option, which is how we link. The error
    was silently ignored by `ld` until more recent versions, which
    complain and fail the build.

--- build/rules.mk
+++ build/rules.mk
@@ -13,8 +13,8 @@ ifeq ($(origin CXX),default)
 CXX     := $(shell $(SHELL) build/findgcc.sh $(CXX))
 endif
 else
-CC      = $(CCPREFIX)cc
-CXX     = $(CCPREFIX)c++
+CC      = $(CCPREFIX)gcc
+CXX     = $(CCPREFIX)g++
 endif
 LD      = $(CCPREFIX)ld
 OBJCOPY = $(CCPREFIX)objcopy
@@ -57,7 +57,7 @@ endif
 
 # Linker flags
 LDFLAGS := $(LDFLAGS) -Os --gc-sections -z max-page-size=0x1000 \
-	-static -nostdlib -nostartfiles
+	-static -nostdlib
 LDFLAGS	+= $(shell $(LD) -m elf_x86_64 --help >/dev/null 2>&1 && echo -m elf_x86_64)
 
 QUIETOBJCOPY = sh build/quietobjcopy.sh $(OBJCOPY)
